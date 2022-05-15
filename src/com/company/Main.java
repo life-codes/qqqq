906ff66d3befa16b94df2a922b1eb6ab1c86f92a
fengyu <995056126@qq.com>
2022-05-15 22:58:18 +0800

    第二次提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("4545");
+        HashMap<String, Integer> objectObjectHashMap = new HashMap<>();
     }
 }
